c5293916d46b1f13c83a1412a1a6e90a03fb8d1e
Chan Nyein Ko Ko <32431724+channyein87@users.noreply.github.com>
2023-11-19 03:14:44 +0000

    multiple iam role

--- module/addons.tf
+++ module/addons.tf
@@ -53,7 +53,7 @@ module "eks_blueprints_addons" {
 }
 
 resource "aws_iam_role" "ebs_addon" {
-  name = "ebs-addon-role"
+  name_prefix = "ebs-addon-role-"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
